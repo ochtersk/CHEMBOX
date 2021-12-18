1b7bac597a785a5f556cc5523983579ac350ff94
Joe Ochterski <ochtersk@gmail.com>
2021-12-18 12:53:57 -0500

    added get_metric_prefix_set (needs more testing) added test for incompatible units in do_convert added get_conversion factor and tests

--- chemplates/tests/test_unit_conversion_utils.py
+++ chemplates/tests/test_unit_conversion_utils.py
@@ -1,5 +1,6 @@
 from CHEMBOX.chemplates.unit_conversion_utils import *
 import CHEMBOX.refdata.DataValue as DV
+import pint
 import pytest
 
 
@@ -26,7 +27,38 @@ def test_do_convert(inputDVstr,finalunitsstr,answerDVstr):
     convertedDV = do_convert(inputDV,finalunits)
     assert str(answerDV)==str(convertedDV)
 
-    # TO DO:
+    #XXX TO DO:
     # make conversions exact
-    # future: make arbitrary units work
+    # DONE: make arbitrary units work
     #get datavalue to take strings with no space delimiters
+
+@pytest.mark.parametrize("inputunitsstr,finalunitsstr,answerstr", [
+    ("feet", "inches", "12"),
+    ("inches", "feet", "0.083"),
+    ])
+def test_get_conversion_factor(inputunitsstr,finalunitsstr,answerstr):
+    conversion_factor = get_conversion_factor(inputunitsstr,finalunitsstr)
+    assert str(conversion_factor.magnitude)==answerstr
+
+@pytest.mark.parametrize("inputDVstr,finalunitsstr,answerDVstr", [
+    ("6.0 ft", "liters", "no answer"),
+    ])
+def test_do_convert_broken_calls(inputDVstr,finalunitsstr,answerDVstr):
+    inputDV=DV.DataValue(inputDVstr)
+    finalunits=finalunitsstr
+    with pytest.raises(pint.errors.DimensionalityError) as excinfo:
+        convertedDV = do_convert(inputDV,finalunits)
+        assert "DimensionalityError" in str(excinfo.value)
+
+# XXX needs more test on other sets
+@pytest.mark.parametrize("setname,abbrev,lookfor,shouldntsee", [
+    ("3-3", "NO", ["deca","centi"], ["da","m"]),
+    ("3-3", "YES", ["c"], ["centi","y","yotta"]),
+    ("3-3", "BOTH", ["c","centi"], ["y","yotta"]),
+    ])
+def test_get_metric_prefix_set(setname,abbrev,lookfor,shouldntsee):
+    prefix_set = get_metric_prefix_set(set_label=setname, abbreviations=abbrev)
+    for item in lookfor:
+        assert (item in prefix_set), f"can't find {item} in {*prefix_set,}"
+    for item in shouldntsee:
+        assert (item not in prefix_set), f"found {item} in {*prefix_set,}"
--- chemplates/unit_conversion_utils.py
+++ chemplates/unit_conversion_utils.py
@@ -2,6 +2,7 @@ from pprint import pformat
 from CHEMBOX.refdata import ureg, Q_
 import CHEMBOX.refdata.DataValue as DV
 import CHEMBOX.sigfig.createsf as SF
+import CHEMBOX.chemplates.units_sets as UNIT_SETS
 
 def create_conversion(PintDefinition):
     """create_conversion creates a units conversion
@@ -39,7 +40,7 @@ def do_convert(inputDV,finalunits):
     Parameters
     ----------
     inputDV : a DataValue to be converted to different units
-    finalunits: the units to convert to
+    finalunits: a string representing the units to convert to
 
     Returns
     -------
@@ -47,16 +48,82 @@ def do_convert(inputDV,finalunits):
 
     Raises
     ------
-    xAttributeError
-        xif no attribute, value pair or dict is specified.
+    pint.errors.DimensionalityError
+        if there's no way to convert the units
 
     """
-    # put the conversion factors in the right place
-    # the destination units should be in convfactorDV[1] and the starting units in
-    # convfactorDV[0]
     verbose = True
-
-
     resultDV = inputDV.quantity.to(finalunits)
-
     return resultDV
+
+def get_conversion_factor(input_units,output_units):
+    """get_conversion_factor determine the conversion factor between units
+
+    Parameters
+    ----------
+    input_units : a string representing the units to convert from
+    output_units: a string representing the units to convert to
+
+    Returns
+    -------
+    a DataValue with the units conversion factor (1 in_unit = xx out_units)
+
+    Raises
+    ------
+
+    """
+    inputDV=DV.DataValue(" ".join(("1.0",input_units)))
+    resultDV = do_convert(inputDV,output_units)
+    return resultDV
+
+
+def get_metric_prefix_set(set_label=None, abbreviations="YES"):
+    """get_metric_prefix_set get a set of metric prefixes to convert from/to
+
+    Parameters
+    ----------
+    set_label 3-3|15-15|24-24 to get sets of metric prefixes ("3-3"=10^3- to 10^-3, etc)
+    abbreviations YES to return just abbreviations (default), NO for full prefix, BOTH returns both
+
+    Returns
+    -------
+    a set of strings which can be used as metric prefixes
+
+    Raises
+    ------
+
+    """
+    prefix_set =set();
+    if (set_label) in UNIT_SETS.prefixes:
+        if abbreviations == "YES":
+            prefix_set = set(UNIT_SETS.prefixes[set_label].values())
+        elif abbreviations == "NO":
+            prefix_set = set(UNIT_SETS.prefixes[set_label].keys())
+        else:
+
+            prefix_set = set(UNIT_SETS.prefixes[set_label].keys()).union(\
+                         set(UNIT_SETS.prefixes[set_label].values()))
+    else:
+        raise KeyError(f"key {set_label} does not exist in prefixes")
+    return prefix_set
+
+def get_units_set():
+    """get_units_set get a set of units to convert from/to
+
+    Parameters
+    ----------
+    dimensionality : (optional) dimensionality of units
+    compatible_with : (optional) units to make the set compatible with
+    use_metric : (optional) BASIC|EXTENDED|ALL to include sets of metric prefixes
+    SI_only : (optional) use only SI units
+    predefined_set : (optional) the name of a predefined set of
+
+    Returns
+    -------
+    a set of strings which can be used as units
+
+    Raises
+    ------
+
+    """
+    pass
